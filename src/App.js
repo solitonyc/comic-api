0ad17c35f0d08d6a846aa8e4eee3484ebeb9088b
solitonyc <solito-air@ool-ad0204bf.dyn.optonline.net>
2019-03-25 20:32:01 -0400

    fetch works and renders

--- src/App.js
+++ src/App.js
@@ -2,11 +2,46 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <p>Marvel Comics API</p>
-      </div>
+  constructor(props) {
+    super(props);
+      this.state = {
+        name: '',
+        apiData: null
+      }
+  }
+  
+componentDidMount() {
+  this.getApi()
+}
+
+getApi() {
+  fetch(`https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=hu&apikey=52e4b1c55a34b26844f88e80975dd286`)
+  .then(response => {
+    return response.json();
+  })
+  .then((json) => {
+    this.setState({
+      apiData: json.data.results
+    })
+  })
+}
+
+handleSubmit(e) {
+  e.preventDefault()
+  this.getApi()
+}
+
+printApiData() {
+  if(this.state.apiData){
+  return this.state.apiData.map( apiData => ( <h2 key={apiData.id}>{apiData.name}</h2> ))
+  }
+}
+
+render() {
+  console.log(this.state.apiData)
+    return ( <div className="app-page">
+      {this.printApiData()}
+    </div>
     );
   }
 }
